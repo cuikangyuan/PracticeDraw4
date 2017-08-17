501e327e243c7f960c765b8505b9cf8384b441a5
cuikangyuan <cuikangyuan@outlook.com>
2017-08-17 18:30:05 +0800

    add code

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
@@ -3,6 +3,7 @@ package com.hencoder.hencoderpracticedraw4.practice;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Camera;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
@@ -37,8 +38,35 @@ public class Practice12CameraRotateFixedView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        Camera camera = new Camera();
+        int cx1 = point1.x + bitmap.getWidth() / 2;
+        int cy1 = point1.y + bitmap.getHeight() / 2;
+        int cx2 = point2.x + bitmap.getWidth() / 2;
+        int cy2 = point2.y + bitmap.getHeight() / 2;
 
+
+        canvas.save();
+
+        camera.save();
+        camera.rotateX(20);
+        canvas.translate(cx1, cy1);
+        camera.applyToCanvas(canvas);
+        canvas.translate(-cx1, -cy1);
+        camera.restore();
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+
+
+        canvas.save();
+
+        camera.save();
+        camera.rotateY(20);
+        canvas.translate(cx2, cy2);
+        camera.applyToCanvas(canvas);
+        canvas.translate(-cx2, -cy2);
+        camera.restore();
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
     }
 }
